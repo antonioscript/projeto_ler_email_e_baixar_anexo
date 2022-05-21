a350ad467e83918d245884b2e6b3d1cc3dc1d2d9
Antônio Rocha <antoniojunior159@gmail.com>
2022-05-20 21:39:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+
 from imap_tools import MailBox, AND
 
-usuario = "antoniojunior159@gmail.com"
-senha = "simboloperdido852"
+usuario = "seu_email"
+senha = "sua_senha"
 
 meu_email = MailBox("imap.gmail.com").login(usuario, senha)
 
 #Pegar email que foram enviados por um remetente específico
-# pegar emails com um anexo específico
-lista_emails = meu_email.fetch(AND(from_="antoniojunior159@gmail.com"))
+lista_emails = meu_email.fetch(AND(from_="seu_email"))
 for email in lista_emails:
     if len(email.attachments) > 0:
         for anexo in email.attachments:
@@ -16,4 +16,3 @@ for email in lista_emails:
                 print(anexo.payload)
                 with open("Teste.doc", 'wb') as arquivo_docx:
                     arquivo_docx.write(anexo.payload)
-#Pegar anexo de um emal
